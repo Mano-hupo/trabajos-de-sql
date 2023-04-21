0404ce4afc11b753b8b8ddcd794959f6039a7d0e
Mano-hupo <129344152+Mano-hupo@users.noreply.github.com>
2023-04-20 19:21:48 -0500

    primer archivo

---
+++ gestion_de_data.sql
@@ -0,0 +1,10 @@
+-- ******************************************************
+-- * SCRIPT PARA GESTIÓN DE LA DATA DE LA BD DB_INVENT *
+-- ******************************************************
+INSERT INTO tab_prov VALUES(1,'Proveedor 1','Dirección P1',2111111,30,ROW(6887766,2,TRUE,'HDN965'));
+INSERT INTO tab_prov VALUES(2,'Proveedor 2','Dirección P2',2222222,19,ROW(6212221,NULL,NULL,'ABC123'));
+INSERT INTO tab_prov VALUES(3,'Proveedor 3','Dirección P3',2333333,25,ROW(6232323,NULL,NULL,'XYX321'));
+INSERT INTO tab_prov VALUES(3,'Proveedor 3','Dirección P3',2333333,25,ROW(6232323,NULL,NULL,'XYX321'));
+
+ INSERT INTO "nombre_tabla" VALUES ("valor1", "valor2", ..
+
