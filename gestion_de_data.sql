4282ee316fbf76d4547520cabb5d028c700aeecf
unknown <Sena@BUCDFPSEFSD054.FormacionSena.RED>
2023-04-20 19:28:00 -0500

    cambios

---
+++ gestion_de_data.sql
@@ -0,0 +1,16 @@
+-- ******************************************************
+-- * SCRIPT PARA GESTIÓN DE LA DATA DE LA BD DB_INVENT *
+-- ******************************************************
+INSERT INTO tab_prov VALUES(1,'Proveedor 1','Dirección P1',2111111,30,ROW(6887766,2,TRUE,'HDN965'));
+INSERT INTO tab_prov VALUES(2,'Proveedor 2','Dirección P2',2222222,19,ROW(6212221,NULL,NULL,'ABC123'));
+INSERT INTO tab_prov VALUES(3,'Proveedor 3','Dirección P3',2333333,25,ROW(6232323,NULL,NULL,'XYX321'));
+INSERT INTO tab_prov VALUES(4,'Proveedor 4','Dirección P4',2444444,25,ROW(6232323,NULL,NULL,'XYX321'));
+INSERT INTO tab_prov VALUES(5,'Proveedor 5','Dirección P5',2555555,30,ROW(6887766,2,TRUE,'HDN965'));
+INSERT INTO tab_prov VALUES(6,'Proveedor 6','Dirección P6',2666666,19,ROW(6212221,NULL,NULL,'ABC123'));
+INSERT INTO tab_prov VALUES(7,'Proveedor 7','Dirección P7',7777777,25,ROW(6232323,NULL,NULL,'XYX321'));
+INSERT INTO tab_prov VALUES(8,'Proveedor 8','Dirección P8',2888888,25,ROW(6232323,NULL,NULL,'XYX321'));
+
+
+
+--  INSERT INTO "nombre_tabla" VALUES ("valor1", "valor2", ..
+
